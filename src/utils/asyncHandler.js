b5a3017a4017d017d120c842522bb72fa983e7db
Ahmad Waseem <awtabassum@gmail.com>
2024-06-25 09:52:21 +0500

    Made util files like async handler, ApiResponse and ApiError

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// just to understand following we divide the function in 3 steps as follow
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => ()=>{}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
